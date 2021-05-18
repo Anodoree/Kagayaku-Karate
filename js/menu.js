d821884e2b24b3bed3d063b045dfc435664945d2
Anodoree <emfwws@gmail.com>
2021-05-18 17:22:12 +0300

    edit nav-btn, article__text-link; del imgs transform

--- js/menu.js
+++ js/menu.js
@@ -5,4 +5,7 @@ document.getElementById("nav-btn").onclick = function() {
 function open() {
     document.getElementById("mobile-header__top").classList.toggle("mobile-header__top--active");
     document.getElementById("mobile-nav").classList.toggle("mobile-nav--active");
+    document.getElementById("nav-btn-1").classList.toggle("mobile-header__btn-span-1--active");
+    document.getElementById("nav-btn-2").classList.toggle("mobile-header__btn-span-2--active");
+    document.getElementById("nav-btn-3").classList.toggle("mobile-header__btn-span-3--active");
 }
